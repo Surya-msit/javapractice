08ef44e17e8d0b8805b76832e6926574cdfe6110
Surya-msit <55694765+Surya-msit@users.noreply.github.com>
2023-02-20 23:10:00 +0530

    Add files via upload

---
+++ occofchar.java
@@ -0,0 +1,31 @@
+package demopkg;
+
+import java.util.Scanner;
+
+public class occofchar {
+    public static void main(String args[]) {
+        String str;
+        int i, len, counter[] = new int[256];
+ 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Please enter a string : ");
+        str = scanner.nextLine();
+ 
+        //String length
+        len = str.length();
+ 
+        // loop through the string 
+        // and count frequency of every character 
+        // and store it in counter array
+        for (i = 0; i < len; i++) {
+            counter[(int) str.charAt(i)]++;
+        }
+        
+        // Print Frequency of characters
+        for (i = 0; i < 256; i++) {
+            if (counter[i] != 0) {
+                System.out.println((char) i + " --> " + counter[i]);
+            }
+        }
+    }
+}
